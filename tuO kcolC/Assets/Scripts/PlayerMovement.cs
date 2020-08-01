3ebe1bb4fd98844c782fa7046993fa2ba267e744
bobbyhead333 <mikeandike277@gmail.com>
2020-08-01 16:11:47 -0500

    Audio Manager
    Implemented audio manager and "game master" for later use

---
+++ tuO kcolC/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float moveSpeed = 10f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 0.0001f;
+
+    public Transform groundCheck;
+    public float groundDistance = 1f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float playerX = Input.GetAxis("Horizontal");
+        float playerZ = Input.GetAxis("Vertical");
+
+        Vector3 movement = transform.right * playerX + transform.forward * playerZ;
+
+        controller.Move(movement * moveSpeed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
