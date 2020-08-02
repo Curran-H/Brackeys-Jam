35e306069591bb83e18c78bfbbd93eca777617f3
1F1ERCE1 <helmichcurran@gmail.com>
2020-08-02 12:38:26 -0500

    Unsustainable Agony

---
+++ tuO kcolC/Assets/CharControl.cs
@@ -0,0 +1,116 @@
+﻿//Huge thanks and credit to Swindle Creative
+//https://www.youtube.com/watch?v=DzO270DH0tk
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharControl : MonoBehaviour
+{
+    #region Variables
+    public Transform camera;
+    public Rigidbody rb;
+
+    #region CameraVariables
+    public float rotationSmoothSpeed = 10f;
+    public float camRotationSpeed    = 5f;
+    public float cameraMinimumY      = -90f;
+    public float cameraMaximumY      = 90f;
+#endregion
+
+    #region MovementVariables
+    public float walkSpeed   = 9f;
+    public float runSpeed    = 14f;
+    public float maxSpeed    = 20f;
+    public float jumpPower   = 30f;
+    #endregion
+
+    #region Gravity
+    public float extraGravity = 45f;
+    #endregion
+
+    #region Directions and Rotations
+    float    bodyRotationX;
+    float    camRotationY;
+    Vector3  directionIntentX;
+    Vector3  directionIntentY;
+    float    speed;
+    #endregion
+
+    public bool grounded;
+    #endregion
+
+    void Update()
+    {
+        LookRotation();
+        Movement();
+        ExtraGravity();
+        GroundCheck();
+        if(grounded && Input.GetButtonDown("Jump"))
+            Jump();
+    }
+
+    void LookRotation()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        //Get Camera and body rotational values
+        bodyRotationX    += Input.GetAxis("Mouse X") * camRotationSpeed;
+        camRotationY     += Input.GetAxis("Mouse Y") * camRotationSpeed;
+
+        //Stop our Camera from rotating 360 degrees
+        camRotationY = Mathf.Clamp(camRotationY, cameraMinimumY, cameraMaximumY);
+
+        //Create rotation targets and handle rotations of the body and camera
+        Quaternion camTargetRotation     = Quaternion.Euler(-camRotationY, 0, 0);
+        Quaternion bodyTargetRotation    = Quaternion.Euler(0, bodyRotationX, 0);
+
+        //Handle rotations
+        transform.rotation = Quaternion.Lerp(transform.rotation, bodyTargetRotation, Time.deltaTime * rotationSmoothSpeed);
+
+        camera.localRotation = Quaternion.Lerp(camera.localRotation, camTargetRotation, Time.deltaTime * rotationSmoothSpeed);
+    }
+
+    void Movement()
+    {
+        //Direction must match camera direction
+        directionIntentX = camera.right;
+        directionIntentX.y = 0;
+        directionIntentX.Normalize();
+
+        directionIntentY = camera.forward;
+        directionIntentY.y = 0;
+        directionIntentY.Normalize();
+
+        //Change our characters velocity in this direction
+        rb.velocity = directionIntentY * Input.GetAxis("Vertical") * speed + directionIntentX * Input.GetAxis("Horizontal") * speed + Vector3.up * rb.velocity.y;
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+
+        //Control our speed based on our movement state
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+        }
+        if (!Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = walkSpeed;
+        }
+    }
+
+    void ExtraGravity()
+    {
+        rb.AddForce(Vector3.down * extraGravity);
+    }
+
+    void GroundCheck()
+    {
+        RaycastHit groundHit;
+        grounded = Physics.Raycast(transform.position, -transform.up, out groundHit, 1.25f);
+    }
+
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+    }
+}
--- tuO kcolC/Assets/Scripts/TestCharacterController.cs
+++ tuO kcolC/Assets/Scripts/TestCharacterController.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class TestCharacterController : MonoBehaviour
 {
+    #region Variables
     [SerializeField]
     private Rigidbody playerRigidBody;
 
     private Vector3 inputVector;
     public float speed;
     public float jumpPower;
+    public float gravity = -9.81f;
+    #endregion
 
     void Start()
     {
@@ -18,12 +21,21 @@ public class TestCharacterController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        inputVector = new Vector3(Input.GetAxisRaw("Horizontal") * speed, playerRigidBody.velocity.y, Input.GetAxisRaw("Vertical") * speed);
-        playerRigidBody.velocity = inputVector;
+        float xAxis = Input.GetAxisRaw("Horizontal");
+        float zAxis = Input.GetAxisRaw("Vertical");
+        float y = Mathf.Sqrt(jumpPower * -2f * gravity);
+
+        Vector3 move = (transform.right * xAxis) + (transform.forward * zAxis);
+        playerRigidBody.velocity = move * speed; //Make it move
+
+        //inputVector = new Vector3(xAxis * speed, playerRigidBody.velocity.y, zAxis * speed); //Get "where to move"
+        //playerRigidBody.velocity = inputVector; //Make it move
+
         if (Input.GetButtonDown("Jump"))
         {
-            playerRigidBody.AddForce(0, jumpPower, 0, ForceMode.Impulse);
-            Debug.Log("Jump Success");
-        }
+            //playerRigidBody.AddForce(0, jumpPower, 0, ForceMode.Impulse);
+            playerRigidBody.AddForce(Vector3.up *jumpPower, ForceMode.Impulse);
+            Debug.Log("Jumped");
+        } //Jump
     }
 }
