d50cbb862f95651e4dcf2e5041f870031ffe102f
bobbyhead333 <mikeandike277@gmail.com>
2020-08-06 15:26:04 -0500

    Vents and Other Changes
    Programmed vents to push player in direction of current, changed grab distance and jump variables of player prefabs.

---
+++ tuO kcolC/Assets/AirVent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirVent : MonoBehaviour
+{
+    public bool airUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Vent collision detected!");
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "IsPickupable" || other.gameObject.tag == "IsInfestible")
+        {
+            if(other.gameObject.tag == "Player")
+                other.gameObject.GetComponent<CharControl>().inVent = true;
+            Debug.Log("Vent collision tag detection successful!");
+            other.gameObject.transform.position += gameObject.transform.up * 0.2f;
+            other.gameObject.GetComponent<Rigidbody>().useGravity = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<CharControl>().inVent = false;
+            other.gameObject.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+}
