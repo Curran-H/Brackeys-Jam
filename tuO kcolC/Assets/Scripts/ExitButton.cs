ca1b9b345573500d17711a91d27cb2070a35ea30
bobbyhead333 <mikeandike277@gmail.com>
2020-08-07 13:57:47 -0500

    Main Menu
    Added main menu with start and exit buttons

---
+++ tuO kcolC/Assets/Scripts/ExitButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitButton : MonoBehaviour
+{
+    public Button startButton;
+
+    // Update is called once per frame
+    void Update()
+    {
+        startButton.onClick.AddListener(exitGame);
+    }
+
+    void exitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ tuO kcolC/Assets/Scripts/StartButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    public Button startButton;
+
+    // Update is called once per frame
+    void Update()
+    {
+        startButton.onClick.AddListener(loadStart);
+    }
+
+    void loadStart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
